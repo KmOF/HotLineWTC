652b6196b0b27a158c7107e7f61586b24ab4e6e9
Gomotso MOFOKENG <gmofoken@e4r11p10.wethinkcode.co.za>
2017-10-07 12:19:55 +0200

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public void StartButton_OnClick(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void QuitButton_OnClick(){
+		Application.Quit();
+	}
+}
