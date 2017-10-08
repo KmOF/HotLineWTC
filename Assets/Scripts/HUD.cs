6fb221105ef7ac565ffc59d73d6a860045f732dc
Gomotso MOFOKENG <gmofoken@e4r11p10.wethinkcode.co.za>
2017-10-08 14:40:23 +0200

    Win/Lose Game state added

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+	public GameObject Win;
+	public GameObject Lose;
+
+	// Use this for initialization
+	void Start () {
+		Win.SetActive(false);
+		Lose.SetActive(false);
+	}
+
+	public void win(){
+		Win.SetActive(true);
+	}
+
+	public void lose(){
+		Lose.SetActive(true);
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,22 @@ public class Player : MonoBehaviour {
 	public Vector3 mousePosition;
 	public GameObject cursor;
 	public GameObject player;
+	public GameObject Win;
+	public GameObject Lose;
 	public static float playerY;
 	public static float playerX;
 	public Cam cam;
 
 	// Use this for initialization
 	void Start () {
-		
+		Win.SetActive(false);
+		Lose.SetActive(false);
 	}
 		
 	// Update is called once per frame
 	void Update () {
 		if (playerY < -12.3863f){
-			
+			Win.SetActive(true);
 		}
 		else if (cam.isGameRunning == true) {
 			moveCursor ();
@@ -31,16 +34,16 @@ public class Player : MonoBehaviour {
 
 		mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		if (Input.GetKey (KeyCode.W)) 
-			player.transform.position += new Vector3 (0, 0.02f, 0);
+			player.transform.position += new Vector3 (0, 0.05f, 0);
 
 		if (Input.GetKey (KeyCode.S)) 
-			player.transform.position += new Vector3 (0, -0.02f, 0);
+			player.transform.position += new Vector3 (0, -0.05f, 0);
 
 		if (Input.GetKey (KeyCode.A)) 
-			player.transform.position += new Vector3 (-0.02f, 0, 0);
+			player.transform.position += new Vector3 (-0.05f, 0, 0);
 
 		if (Input.GetKey (KeyCode.D)) 
-			player.transform.position += new Vector3 (0.02f, 0, 0);
+			player.transform.position += new Vector3 (0.05f, 0, 0);
 
 
 		playerX = player.transform.position.x;
